7ae3b483a0a8476c90f0ddd5a8d72722a41c578c
kingdomunder <king4525@gmail.com>
2021-08-23 21:54:20 +0900

    [add] 모든 DAO에서 selectOne, selectAll 구현

--- src/model/dao/StudentDAO.java
+++ src/model/dao/StudentDAO.java
@@ -72,36 +72,4 @@ public class StudentDAO {
 	}		
 	
 	
-	
-	//수강생 1명 출석정보 select
-//	@Test
-	void getOneAttendance() {
-		EntityManager em = PublicCommon.getEntityManager();
-		
-		Student x = new Student(2, null, null, null, null);
-		
-		Attendance s = (Attendance) em.createNamedQuery("Attendance.findBystudentId").setParameter("studentId", x).getSingleResult();
-		System.out.println(s);
-		
-		
-		em.close();
-		em = null;
-	}
-	
-	
-	
-	//출석 정보 전부 select
-	@Test
-	void getAllAttendance() {
-		EntityManager em = PublicCommon.getEntityManager();
-		
-		List<Attendance> all = em.createNamedQuery("Attendance.findStudentAll").getResultList();
-		all.forEach(v -> System.out.println(v));
-		
-		em.close();
-		em = null;
-		
-	}	
-	
-	
 }
